1a606456af79fd9138e2a75686769b8437baefd2
malistov.artem <malistov.artem@russianpost.ru>
2023-07-16 15:38:48 +0300

    add homework #1

---
+++ project001/index.js
@@ -0,0 +1,10 @@
+let temperatureC = 45;
+let temperatureF = (9 / 5) * temperatureC + 32;
+console.log("\n--- 1 задание ---")
+console.log(`Температура в Цельсиях = ${temperatureC}\u2103
+Температура в Фаренгейтах = ${temperatureF}\u2109 `)
+
+console.log("\n--- 2 задание ---")
+let name = 'Artem'
+let admin = name;
+console.log(`Значение переменной admin = ${admin}`)
