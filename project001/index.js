74e4dbad9d5d238089d3f7fa6961f0a8952455dc
malistov.artem <malistov.artem@russianpost.ru>
2023-07-16 15:55:41 +0300

    add homework #1 remarks

--- project001/index.js
+++ project001/index.js
@@ -8,3 +8,7 @@ console.log("\n--- 2 задание ---")
 let name = 'Artem'
 let admin = name;
 console.log(`Значение переменной admin = ${admin}`)
+
+document.getElementById("temperatureC").innerHTML = `Температура в Цельсиях = ${temperatureC}\u2103`;
+document.getElementById("temperatureF").innerHTML = `Температура в Фаренгейтах = ${temperatureF}\u2109`;
+document.getElementById("admin").innerHTML = `Значение переменной admin = ${admin}`;
